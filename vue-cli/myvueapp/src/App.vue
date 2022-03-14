ea1f6d0b10a128ce16c0f124143d0f3044654330
HtetAungLaing <myueway98@gmail.com>
2022-03-15 02:12:56 +0630

    vue cli add refs,props,emit,slot

--- vue-cli/myvueapp/src/App.vue
+++ vue-cli/myvueapp/src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    <NavBar />
+    <h1>{{ name }}</h1>
+    <button @click="showModal1 = true">Show Modal 1</button>
+    <button @click="showModal2 = true">Show Modal 2</button>
+    <div v-if="showModal1">
+      <ModelBox title="Modal One" content="Hello from modal one" theme="success" @closeModal="showModal1 = false">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, veritatis hic ut saepe sed, aspernatur voluptates, optio nulla atque in ea itaque fugit facere esse inventore at ipsum. Itaque, blanditiis.</p>
+        <template v-slot:extraSlot>Slot with specific name</template>
+      </ModelBox>
+    </div>
+    <div v-if="showModal2">
+      <ModelBox title="Modal two" content="hello from modal two" theme="danger" @closeModal="showModal2 = false">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus magni at aliquam et fugiat animi accusantium nemo repudiandae. Possimus ab omnis magni. Et, qui exercitationem. Sapiente cupiditate perferendis quia labore.</p>
+      </ModelBox>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
+import ModelBox from "./components/ModelBox.vue";
 
 export default {
   data() {
     return {
       name: "kimmich",
+      // title: "Modal Title",
+      // content: "Hello form Modal",
+      showModal1: false,
+      showModal2: false,
     };
   },
 
   components: {
-    NavBar,
+    ModelBox,
   },
 };
 </script>
---
+++ vue-cli/myvueapp/src/components/ModelBox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div :class="['box', { success: theme == 'success', danger: theme === 'danger' }]">
+      <h1>{{ title }}</h1>
+      <p>{{ content }}</p>
+
+      <slot> This is defautl slot text </slot>
+      <slot name="extraSlot" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "content", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+}
+.box {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 5rem;
+}
+.success {
+  background: lightgreen;
+}
+.danger {
+  background: lightcoral;
+}
+</style>
